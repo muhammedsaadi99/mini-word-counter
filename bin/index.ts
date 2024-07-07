0f546de208adc5e33965b03d214cc9a182d6d63f
Muhammad Saad <muhammed.saadi99@gmail.com>
2024-07-07 19:02:07 +0500

    Initial Commit

---
+++ bin/index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+async function main() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'paragraph',
+            message: 'Enter an English paragraph:',
+        }
+    ]);
+
+    const paragraph: string = answers.paragraph;
+    const characterCount = paragraph.replace(/\s+/g, '').length;
+    const wordCount = paragraph.split(/\s+/).filter(word => word.length > 0).length;
+
+    console.log(`Character count (excluding spaces): ${characterCount}`);
+    console.log(`Word count: ${wordCount}`);
+}
+
+main();
